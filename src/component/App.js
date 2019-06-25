d4b2209fd8e4d1ce37a13584c7a4df1e6527e042
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-06-25 17:58:54 +0900

    update

---
+++ src/component/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import intro from "./intro";
+
+class App extends Component {
+  render() {
+    return <intro />;
+  }
+}
+export default App;
---
+++ src/component/intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function intro() {
+  return (
+    <div className="intro">
+      <h1>Paradigm Shift</h1>
+      <p>A free responsive site template designed by</p>
+      <ul>
+        <li>aa</li>
+      </ul>
+    </div>
+  );
+}
+
+export default intro;
